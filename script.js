4de7b1f752828c88511d231d07ef620ea722d054
rainbowlarva <86572604+rainbowlarva@users.noreply.github.com>
2025-03-02 13:46:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('mousemove', (e) => {
-  let x = (e.clientX / window.innerWidth) * 100;
-  let y = (e.clientY / window.innerHeight) * 100;
-
-  // Only apply the parallax effect
-  document.querySelector('.video-background').style.transform = `translate(-50%, -50%) translate(${x / 30}%, ${y / 30}%)`;
-});
+    let x = (e.clientX / window.innerWidth) * 100;
+    let y = (e.clientY / window.innerHeight) * 100;
+  
+    // Apply parallax by changing the background position based on mouse movement
+    document.querySelector('.video-background').style.transform = `translate(${x / 30}%, ${y / 30}%)`;
+  });
