7461d33d345cec27584c968233d939fed66eb6ff
rainbowlarva <86572604+rainbowlarva@users.noreply.github.com>
2025-03-02 13:53:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ window.addEventListener('mousemove', (e) => {
     let y = (e.clientY / window.innerHeight) * 100;
 
     // Apply parallax by changing the background position based on mouse movement
-    document.querySelector('.video-background').style.transform = `translate(-50%, -50%) translate(${x / 30}%, ${y / 30}%)`;
+    document.querySelector('.video-background').style.transform = `translate(${x / 30}%, ${y / 30}%)`;
 });
