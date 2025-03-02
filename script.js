ba02c1433b9e9e939d836ef5d7aa5dfd12f4e898
rainbowlarva <86572604+rainbowlarva@users.noreply.github.com>
2025-03-02 13:40:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ window.addEventListener('mousemove', (e) => {
     let x = (e.clientX / window.innerWidth) * 100;
     let y = (e.clientY / window.innerHeight) * 100;
   
-    document.querySelector('.video-background').style.transform = `translate(-50%, -50%) translate(${x / 20}%, ${y / 20}%)`;
+    document.querySelector('.video-background').style.transform = `translate(-50%, -50%) translate(${x / 30}%, ${y / 30}%)`;
   });
+  
